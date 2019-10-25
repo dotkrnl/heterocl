51f15e9832246851fbebfdbacf2ea928214a78f1
Jason Lau <lau@cs.ucla.edu>
2019-10-25 00:41:40 +0000

    [Backend] Refactor digitrec host code to support new MerlinC backend.

--- samples/digitrec/digitrec_host.cpp
+++ samples/digitrec/digitrec_host.cpp
@@ -16,32 +16,32 @@ int popcount(unsigned long num) {
   return out;
 }
 
-void update_knn(unsigned long *dist, unsigned char *knn_mat, int x, int y) {
+void update_knn(unsigned long dist[10][1800], unsigned char knn_mat[10][3], int x, int y) {
   int id = 0;
   for (int i = 0; i < 3; ++i) {
-    if (knn_mat[x * 3 + i] > knn_mat[x * 3 + id])
+    if (knn_mat[x][i] > knn_mat[x][id])
       id = i;
   }
-  if (dist[x * 1800 + y] < knn_mat[x * 3 + id])
-    knn_mat[x * 3 + id] = dist[x * 1800 + y];
+  if (dist[x][y] < knn_mat[x][id])
+    knn_mat[x][id] = dist[x][y];
   return ;
 }
 
-void default_function(unsigned long test_image, unsigned long *train_image,
-    unsigned char *knn_mat) {
+void default_function(unsigned long test_image, unsigned long train_image[10][1800],
+    unsigned char knn_mat[10][3]) {
 
-  unsigned long diff[18000];
-  unsigned long dist[18000];
+  unsigned long diff[10][1800];
+  unsigned long dist[10][1800];
 
   for (int i = 0; i < 10; ++i) {
     for (int j = 0; j < 3; ++j)
-      knn_mat[i * 3 + j] = 50;
+      knn_mat[i][j] = 50;
 
     for (int j = 0; j < 1800; ++j) {
-      diff[i * 1800 + j] = train_image[i * 1800 + j] ^ test_image;
+      diff[i][j] = train_image[i][j] ^ test_image;
     }
     for (int j = 0; j < 1800; ++j) {
-      dist[i * 1800 + j] = popcount(diff[i * 1800 + j]);
+      dist[i][j] = popcount(diff[i][j]);
     }
     for (int j = 0; j < 1800; ++j) {
       update_knn(dist, knn_mat, i, j);
@@ -53,8 +53,8 @@ void default_function(unsigned long test_image, unsigned long *train_image,
 #ifdef MCC_ACC
 #include MCC_ACC_H_FILE
 #else
-void default_function(unsigned long test_image, unsigned long* train_images,
-    unsigned char* knn_mat);
+void default_function(unsigned long test_image, unsigned long train_images[10][1800],
+    unsigned char knn_mat[10][3]);
 #endif
 #endif
 
@@ -70,6 +70,7 @@ void read_train_file(string filename, unsigned long *train_image) {
   while (!f.eof()) {
     string str;
     f >> str;
+    if (f.eof()) break;
     unsigned long val = strtoul(str.substr(0, str.length() - 1).c_str(),
         NULL, 0);
     train_image[cnt++] = val;
@@ -91,6 +92,7 @@ void read_test_file(string filename, unsigned long *test_image,
   while (!f.eof()) {
     string str;
     f >> str;
+    if (f.eof()) break;
     unsigned long val = strtoul(str.substr(0, str.length() - 2).c_str(),
         NULL, 0);
     test_image[cnt] = val;
@@ -101,18 +103,18 @@ void read_test_file(string filename, unsigned long *test_image,
   return ;
 }
 
-int vote(unsigned char *knn_mat) {
+int vote(unsigned char knn_mat[10][3]) {
   int score[10] = {0};
 
-  for (int i = 0; i < 30; i += 3)
-    sort(knn_mat + i, knn_mat + i + 3);
+  for (int i = 0; i < 10; ++i)
+    sort(knn_mat[i], knn_mat[i] + 3);
 
   for (int i = 0; i < 3; ++i) {
     int m = INT_MAX;
     int id = 0;
     for (int j = 0; j < 10; ++j) {
-      if (knn_mat[j * 3 + i] < m) {
-        m = knn_mat[j * 3 + i];
+      if (knn_mat[j][i] < m) {
+        m = knn_mat[j][i];
         id = j;
       }
     }
@@ -131,24 +133,21 @@ int vote(unsigned char *knn_mat) {
 }
 
 int main(int argc, char **argv) {
-  unsigned long *train_image;
-  unsigned long *test_image;
-  int *test_label;
+  unsigned long train_image[10][1800] = {0};
+  unsigned long test_image[180] = {0};
+  int test_label[180] = {0};
 
 #ifdef MCC_ACC
   __merlin_init(argv[argc-1]);
 #endif
 
   // Prepare data
-  train_image = (unsigned long *)malloc(sizeof(unsigned long) * 10 * 1800);
   for (int i = 0; i < 10; ++i) {
     char str[32];
     sprintf(str, "./data/training_set_%d.dat", i);
-    read_train_file(str, &train_image[i * 1800]);
+    read_train_file(str, train_image[i]);
   }
 
-  test_image = (unsigned long *)malloc(sizeof(unsigned long) * 180);
-  test_label = (int *)malloc(sizeof(int) * 180);
   read_test_file("./data/testing_set.dat", test_image, test_label);
 
   // Compute
@@ -156,8 +155,7 @@ int main(int argc, char **argv) {
   struct timeval tv_start, tv_end;
   double kernel_time = 0.0;
   for (int i = 0; i < 180; ++i) {
-    unsigned char *knn_mat = (unsigned char *)malloc(
-        sizeof(unsigned char) * 3 * 10);
+    unsigned char knn_mat[10][3] = {0};
 
     gettimeofday (&tv_start, NULL);
 #ifdef MCC_ACC
@@ -172,7 +170,7 @@ int main(int argc, char **argv) {
       correct++;
   }
   cout << "Average kernel time: " << kernel_time / 180 << " ms" << endl;
-  cout << (float) correct / 180 << endl;
+  cout << "Accuracy (%): " << (float) correct / 180 * 100 << endl;
 
 #ifdef MCC_ACC
     __merlin_release();
